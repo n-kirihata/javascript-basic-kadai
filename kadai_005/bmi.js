348117576413f86a83fc17700293342eec407e71
n-kirihata <163854084+n-kirihata@users.noreply.github.com>
2024-04-25 20:28:30 +0900

    「kadai_005」の提出

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+// 変数の宣言
+let userBmi;
+let userHeight;
+let userWeight;
+
+// 値の代入
+userWeight = 68;
+userHeight = 1.7;
+userBmi = userWeight / (userHeight * userHeight)
+
+// コンソールへの出力
+console.log(userBmi);
