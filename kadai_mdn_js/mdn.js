0dba4c6fb74a854d30f218f861f7c4895893a40c
n-kirihata <163854084+n-kirihata@users.noreply.github.com>
2024-05-07 20:10:54 +0900

    「kadai_mdn_js」の提出

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,11 @@
+// 年月日の取得
+const date = new Date();
+
+const [month, day, year] = [
+  date.getMonth() +1,
+  date.getDate(),
+  date.getFullYear(),
+];
+
+// 出力
+console.log(year + '年' + month + '月' + day + '日');
